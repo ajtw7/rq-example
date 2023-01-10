63c513155cae756f2e121a5671810763f1b8ca24
mq-jru <andrew@mediaquery.io>
2023-01-10 17:41:57 -0500

    axios

--- src/components/views/SuperHeroes.jsx
+++ src/components/views/SuperHeroes.jsx
@@ -1,4 +1,27 @@
+import { useState, useEffect } from "React";
+import axios from "axios";
+
 export const SuperHeroes = () => {
-    return <div>SuperHeroes</div>;
-  };
-  
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://localhost:4000/superheroes").then((res) => {
+      setData(res.data);
+      setIsLoading(false);
+    });
+  }, []);
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <>
+      <h2>Super Heroes Page</h2>
+      {data.map((hero) => {
+        return <div key={hero.name}>{hero.name}</div>;
+      })}
+    </>
+  );
+};
