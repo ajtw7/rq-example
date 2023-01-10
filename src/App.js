c3138a09d9d6eb44ec1fa79cfe111bc1390ade78
mq-jru <andrew@mediaquery.io>
2023-01-10 15:58:30 -0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import { Home } from "./components/views/Home";
+import { SuperHeroes } from "./components/views/SuperHeroes";
+import { RQSuperHeroes } from "./components/views/RQSuperHeroes";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/superheroes">Traditiona Super Heroes</Link>
+            </li>
+            <li>
+              <Link to="/rq-superheroes">RQ Super Heroes</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/superheroes" element={<SuperHeroes />}/>
+          <Route path="/rq-superheroes" element={<RQSuperHeroes />}/>
+          <Route path="/" element={<Home />}/>
+          
+        </Routes>
+      </div>
+
   );
 }
 
