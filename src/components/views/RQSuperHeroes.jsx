17a90b73671e499909ae5696df1662734cb68a91
mq-jru <andrew@mediaquery.io>
2023-01-10 16:20:07 -0500

    iniial prt 2

---
+++ src/components/views/RQSuperHeroes.jsx
@@ -0,0 +1,4 @@
+export const RQSuperHeroes = () => {
+    return <div>RQSuperHeroes</div>;
+  };
+  
---
+++ src/components/views/SuperHeroes.jsx
@@ -0,0 +1,4 @@
+export const SuperHeroes = () => {
+    return <div>SuperHeroes</div>;
+  };
+  
